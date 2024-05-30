dfe6d3b98665897780b59686ecfa828eec81d375
Pablo <pablo_drgoes@hotmail.com>
2024-05-30 01:51:43 -0300

    Projeto pronto

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import { useState } from "react";
 import Titulo from "./components/Titulo/Titulo";
 import CartaoPrevisao from "./components/Card/CartaoPrevisao";
+import CartaoPrevisaoAtual from "./components/Card/CartaoPrevisaoAtual";
 
 function App() {
   const [previsoes, setPrevisoes] = useState([]);
+  const [previsaoAtual, setPrevisaoAtual] = useState(null);
   const [cidade, setCidade] = useState('São Paulo');
 
   const chamarApi = () => {
@@ -18,6 +20,14 @@ function App() {
         const previsoesDiarias = dadosPrevisao.list.filter((_, index) => index % 8 === 0).slice(0, 5);
         setPrevisoes(previsoesDiarias);
       });
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&lang=pt_br&units=metric&appid=777fd6c175f16899b669ab9b22be7638`
+    )
+      .then((resposta) => resposta.json())
+      .then((dadosAtuais) => {
+        setPrevisaoAtual(dadosAtuais);
+      });
   };
 
   const handleCidadeChange = (evento) => {
@@ -30,6 +40,14 @@ function App() {
 
       <input type="text" placeholder="Insira o nome da cidade" onChange={handleCidadeChange}></input>
       <button onClick={chamarApi}>Buscar</button>
+
+      {previsaoAtual && (
+        <CartaoPrevisaoAtual
+          cidade={cidade}
+          temperatura={previsaoAtual.main.temp}
+          descricao={previsaoAtual.weather[0].description}
+        />
+      )}
 
       <div className="previsoes-container">
         {previsoes.map((previsao, index) => (
@@ -46,3 +64,4 @@ function App() {
 }
 
 export default App;
+
